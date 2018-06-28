bbfb7a0dc38fd98de98b736a1b3fa8b6d7716180
Chya Yan Liaw <cyliaw517@gmail.com>
2018-06-27 22:10:48 -0400

    correct stacked value and label

--- main.m
+++ main.m
@@ -1,19 +1,37 @@
+clear all
+close all
+%% User Input
 NumStacksPerGroup = 3; %for wt%: 1%, 2%, 3%
 NumGroupsPerAxis = 5;  %for t1's: 80s, 90, 100s, 110s, 120s
 NumStackElements = 4;  %for t2's: 0s, 5s, 10s, 15s
 
 % labels to use on tick marks for groups:
 groupLabels = {80; 90; 100; 110; 120};
+% labels to mark each concentration
+barLabels = {'1%', '2%', '3%'};
 
-% experimental data:
+% experimental viscosity data (in mPa*s)
+% different t2s are grouped with square brackets
+% different t1s are separated by semicolons
+% different wt% are separated by commas
 
-stackData = cat(3, [0, 4.25E+03, 0; 5.51E+03, 8.06E+03, 0; 1.17E+04, 6.70E+04, 0; 4.25E+04, 3.25E+05, 0; 0, 8.51E+05, 0], ...
-    [0, 0, 0; 0, 0, 0; 0, 0, 0; 0, 0, 0; 0, 0, 0], ...
-    [0, 0, 0; 0, 0, 0; 1.72E+05, 0, 0; 0, 0, 0; 0, 0, 0], ...
-    [0, 0, 0; 0, 0, 0; 0, 0, 0; 0, 0, 0; 0, 0, 0]);
+t2_0 =  [2, 2, 3; 4, 3, 7; 4, 2, 5; 4, 3, 6; 7, 8, 9];
+t2_5 =  [10, 13, 15; 12, 15, 18; 11, 17, 19; 13, 16, 18; 13, 18, 19];
+t2_10 = [210, 240, 260; 250, 280, 290; 220, 230, 270; 270, 290, 280; 240, 250, 290];
+t2_15 = [3200, 3400, 3500; 3200, 3600, 3800; 3400, 3600, 3900; 3500, 3700, 3900; 3600, 3800, 4000];
+Et2_0 = [1, 1, 1; 1, 1, 1; 1, 1, 1; 1, 1, 1; 1, 1, 1];
+Et2_5 = [1, 1, 1; 1, 1, 1; 1, 1, 1; 1, 1, 1; 1, 1, 1];
+Et2_10 = [1, 1, 1; 1, 1, 1; 1, 1, 1; 1, 1, 1; 1, 1, 1];
+Et2_15 = [1, 1, 1; 1, 1, 1; 1, 1, 1; 1, 1, 1; 1, 1, 1];
 
-plotBarStackGroups(stackData, groupLabels);
-set(gca,'FontSize',12)
-set(gcf,'Position',[100 100 720 650])
-grid on
-set(gca,'Layer','top') % put grid lines on top of stacks
+
+viscosityData_raw = cat(3, t2_0, t2_5, t2_10, t2_15);
+
+%viscosityData = viscosityData_raw(:,:,2:end) - cumsum(viscosityData_raw(:,:,1:end-1), 3)
+viscosityData_diff = viscosityData_raw(:,:,2:end) - viscosityData_raw(:,:,1:end-1);
+viscosityData = cat(3,viscosityData_raw(:,:,1), viscosityData_diff);
+errorData = cat(3, Et2_0, Et2_5, Et2_10, Et2_15);
+viscosityData(viscosityData<0) = 0;
+
+plotBarStackGroups(viscosityData, viscosityData_raw, errorData, groupLabels, barLabels);
+
--- plotBarStackGroups.m
+++ plotBarStackGroups.m
@@ -1,11 +1,11 @@
-function [] = plotBarStackGroups(stackData, groupLabels)
+function [] = plotBarStackGroups(stackData, viscosityData_raw, errorData, groupLabels, barLabels)
 %% Parameters: 
 %%      stackData is a 3D matrix (i.e., stackData(i, j, k) => (Group, Stack, StackElement)) 
 %%      groupLabels is a CELL type (i.e., { 'a', 1 , 20, 'because' };)
 %% 
 NumGroupsPerAxis = size(stackData, 1);
 NumStacksPerGroup = size(stackData, 2);
-
+%barLabels={'1%', '2%', '3%'};
 % Count off the number of bins
 groupBins = 1:NumGroupsPerAxis;
 MaxGroupWidth = 0.75; % Fraction of 1. If 1, then we have all bars in groups touching
@@ -13,43 +13,50 @@ groupOffset = MaxGroupWidth/NumStacksPerGroup;
 figure
     hold on;
 for i=1:NumStacksPerGroup
-
+    % for each concentration
     Y = squeeze(stackData(:,i,:));
+    EY = squeeze(errorData(:,i,:));
+    Y_raw = squeeze(viscosityData_raw(:,i,:));
     
     % Center the bars:
     internalPosCount = i-((NumStacksPerGroup+1)/2);
     
     % Offset the group draw positions:
-    groupDrawPos = (internalPosCount)*groupOffset+groupBins;
+    groupDrawPos = (internalPosCount)*groupOffset+groupBins
     
     h(i,:) = bar(Y, 'stacked');
+    for k = 1:5
+    errorbar(repmat(groupDrawPos(k), 1, 4), Y_raw(k,:), EY(k,:), 'LineStyle','none');
+    end
     set(h(i,1),'facecolor','b','edgecolor','k'); 
     set(h(i,2),'facecolor','c','edgecolor','k'); 
     set(h(i,3),'facecolor','y','edgecolor','k'); 
     set(h(i,4),'facecolor','r','edgecolor','k');
     set(h(i,:),'BarWidth',groupOffset);
     set(h(i,:),'XData',groupDrawPos); 
-end
-
-% Add bar labels:
-barLabels={'1%', '2%', '3%'};
-
-% Bar labeling:
+    
+    % Bar labeling:
 for j=1:NumGroupsPerAxis
     text(groupDrawPos(j),sum(Y(j,:),2),... 
         barLabels{i},'VerticalAlignment','bottom','HorizontalAlignment','center');
 end
+
+end
+
+% Add bar labels:
 
 hold off;
 set(gca,'XTickMode','manual');
 set(gca,'XTick',1:NumGroupsPerAxis);
 set(gca,'XTickLabelMode','manual');
 set(gca,'XTickLabel',groupLabels);
+set(gca,'Yscale', 'log')
 
 % Add axis labels:
 xlabel('t1 (s)')
 ylabel('viscosity (mPa*s)')
 
 % Add a legend:
-legend('t2 = 0s', 't2 = 5s', 't2 = 10s', 't2 = 15s')
+leg = legend('t2 = 0s', 't2 = 5s', 't2 = 10s', 't2 = 15s')
+set(leg, 'location', 'bestoutside')
 end 
