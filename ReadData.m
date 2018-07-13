225293846e45ccfe85b08987fe266acc32b0e60d
Jorge Pereyra <jrp77@njit.edu>
2018-07-12 20:57:09 -0400

    option to omit data via highest value

--- ReadData.m
+++ ReadData.m
@@ -1,14 +1,16 @@
 clear all
 close all
-%% Load Previous Data
-prompt = 'Load Previous Data? ';
+clc
+
+%% load previous data
+prompt = 'load previous data? yes or no? '; %place an '!' at the end when asnwering 'no'
 answer = input(prompt, 's')
 if answer == 'yes'
     load viscosity.mat
     load error.mat
     
-elseif answer == 'no'
-    t2_0 = zeros(3,3);
+elseif answer == 'no!'
+    t2_0  = zeros(3,3);
     t2_10 = zeros(3,3);
     t2_15 = zeros(3,3);
     t2_20 = zeros(3,3);
@@ -21,18 +23,17 @@ elseif answer == 'no'
     errorData = cat(3, t2_0, t2_10, t2_15, t2_20);
 end
 
-%% User input
+%% enter solution condition
 for N = 1:numel(viscosityData_raw)
-    
     prompt = 'EGDMA concentration = ';
     Conc = input(prompt)
     prompt = 't1 = ';
     t1 = input(prompt)
     prompt = 't2 = ';
     t2 = input(prompt)
-    filepath = uigetdir
-    
-%% Fitting
+
+    %% fitting
+    filepath = uigetdir('Selected Rheology Data for Project (as of 07-03)')
     fil = fullfile(filepath,'*.csv')
     d = dir(fil)
     fittype = 'a*[(1 + (b*x)^2)^((n-1)/2)]';
@@ -46,21 +47,33 @@ for N = 1:numel(viscosityData_raw)
         figure
         plot(rate, viscosity,'o')
         set(gca, 'YScale', 'log', 'XScale', 'log')
-        %pause
         [critX,critY] = ginput(1);
         [f1,gof] = fit(rate,viscosity,fittype, 'startpoint', [critY, 0.05, 0.5],...
             'exclude', rate < critX);
         hold on
         plot(f1)
         c = coeffvalues(f1);
-        fitResult = [fitResult; c(1)];
-        
+        fitResult = [fitResult; c(1)] 
     end
     
-    avg = mean(fitResult)
-    S = std(fitResult)
+    %ask if need to modify data
+    prompt = 'need to modify data? yes or no? ';
+    answer = input(prompt, 's')
+    if answer == 'yes'
+        fitResultModified = fitResult;
+        outlier_removal = 'enter row number of outlier to be removed = ';
+        outlier_val = input(outlier_removal)
+        fitResultModified(outlier_val) = [];
+        
+        avg = mean(fitResultModified)
+        S = std(fitResultModified)
+    else answer == 'no!';
+        avg = mean(fitResult)
+        S = std(fitResult)
+    end
     
-%% Assign Value to Matrix
+    %% assign fit result to matrix
+    %t2 assignment
     if t2 == 0
         m = 1;
     elseif t2 == 10
@@ -71,6 +84,7 @@ for N = 1:numel(viscosityData_raw)
         m = 4;
     end
     
+    %EGDMA concentration assignment
     if Conc == 1
         colmn = 1;
     elseif Conc == 2
@@ -79,6 +93,7 @@ for N = 1:numel(viscosityData_raw)
         colmn = 3;
     end
     
+    %t1 assignment
     if t1 == 100
         row = 1;
     elseif t1 == 110
